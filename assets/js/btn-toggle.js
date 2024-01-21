5f5cc2cd16a3e278152615ae5fcc2f60a8c7a2aa
biancassantos <biancassantos89@gmail.com>
2024-01-21 12:03:48 -0300

    complete header

--- assets/js/btn-toggle.js
+++ assets/js/btn-toggle.js
@@ -1,4 +1,12 @@
 function openMenu() {
     let mobileMenu = document.querySelector('.menu-mobile-list')
+    let icon = document.querySelector('.menu')
     mobileMenu.classList.toggle('open')
+    if (icon.classList.contains('bi-list')) {
+        icon.classList.remove('bi-list')
+        icon.classList.add('bi-x')
+    } else {
+        icon.classList.remove('bi-x')
+        icon.classList.add('bi-list')
+    }
 }
